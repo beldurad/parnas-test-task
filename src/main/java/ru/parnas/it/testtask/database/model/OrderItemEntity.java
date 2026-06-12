c723936d668878d57de7af89c3a2ca2073d3168f
beldurad <bulatkash1788@gmail.com>
2026-06-12 18:20:48 +0300

    database models

---
+++ src/main/java/ru/parnas/it/testtask/database/model/OrderItemEntity.java
@@ -0,0 +1,31 @@
+package ru.parnas.it.testtask.database.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name", nullable = false, length = 255)
+    private String productName;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Column(name = "price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private OrderEntity order;
+}
---
+++ src/main/java/ru/parnas/it/testtask/dictionary/OrderStatus.java
@@ -0,0 +1,8 @@
+package ru.parnas.it.testtask.dictionary;
+
+public enum OrderStatus {
+    CREATED,
+    PROCESSING,
+    COMPLETED,
+    CANCELED
+}
