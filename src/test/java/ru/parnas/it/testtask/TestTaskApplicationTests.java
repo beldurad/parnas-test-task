e4c50b5cff498a0ef195eb99557eae5b78c1aeb5
beldurad <bulatkash1788@gmail.com>
2026-06-12 17:25:54 +0300

    project init

---
+++ src/test/java/ru/parnas/it/testtask/TestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.parnas.it.testtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
