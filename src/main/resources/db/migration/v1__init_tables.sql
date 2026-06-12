81ff16ff2ab9d8ffd8ba08b19e3433a7be2881ff
beldurad <bulatkash1788@gmail.com>
2026-06-12 18:20:09 +0300

    migration sql file

---
+++ src/main/resources/db/migration/v1__init_tables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE orders (
+                        id UUID PRIMARY KEY,
+                        customer_name VARCHAR(255) NOT NULL,
+                        order_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+                        status VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE order_items (
+                             id BIGSERIAL PRIMARY KEY,
+                             product_name VARCHAR(255) NOT NULL,
+                             quantity INTEGER NOT NULL,
+                             price NUMERIC(10, 2) NOT NULL,
+                             order_id UUID NOT NULL,
+
+                             CONSTRAINT fk_order_items_orders
+                                 FOREIGN KEY (order_id)
+                                     REFERENCES orders (id)
+                                     ON DELETE CASCADE
+);
+
+CREATE INDEX idx_order_items_order_id ON order_items(order_id);
