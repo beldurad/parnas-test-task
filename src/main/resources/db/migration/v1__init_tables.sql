c9ec2e82ef37133ec65548b0d2be62941cc3351f
beldurad <bulatkash1788@gmail.com>
2026-06-12 20:07:24 +0300

    sql indexes update

--- src/main/resources/db/migration/v1__init_tables.sql
+++ src/main/resources/db/migration/v1__init_tables.sql
@@ -18,4 +18,8 @@ CREATE TABLE order_items (
                                      ON DELETE CASCADE
 );
 
+CREATE INDEX idx_orders_status ON orders(status);
+
+CREATE INDEX idx_orders_customer_name ON orders(customer_name);
+
 CREATE INDEX idx_order_items_order_id ON order_items(order_id);
