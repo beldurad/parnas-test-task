f97bf1f71c50e15fce269eb729f38baadc1a86aa
beldurad <bulatkash1788@gmail.com>
2026-06-13 07:48:13 +0300

    rabbit mq

--- src/main/java/ru/parnas/it/testtask/domain/service/OrderService.java
+++ src/main/java/ru/parnas/it/testtask/domain/service/OrderService.java
@@ -2,6 +2,7 @@ package ru.parnas.it.testtask.domain.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,10 @@ import ru.parnas.it.testtask.dictionary.OrderStatus;
 import ru.parnas.it.testtask.domain.model.Order;
 import ru.parnas.it.testtask.exception.OrderNotFoundException;
 import ru.parnas.it.testtask.mapper.OrderMapper;
+import ru.parnas.it.testtask.mq.RabbitConfig;
+import ru.parnas.it.testtask.mq.dto.OrderCreatedEvent;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -21,6 +25,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
+    private final RabbitTemplate rabbitTemplate;
 
     @Transactional
     public Order createOrder(Order order) {
@@ -29,9 +34,26 @@ public class OrderService {
         order.setOrderDate(LocalDateTime.now());
         order.setStatus(OrderStatus.CREATED);
 
-        return orderMapper.toOrder(
+        BigDecimal totalAmount = order.getItems().stream()
+                .map(item -> item.getPrice().multiply(BigDecimal.valueOf(item.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        Order savedOrder = orderMapper.toOrder(
                 orderRepository.save(orderMapper.toOrderEntity(order))
         );
+
+        OrderCreatedEvent event = new OrderCreatedEvent(
+                savedOrder.getId(),
+                savedOrder.getCustomerName(),
+                totalAmount
+        );
+
+        rabbitTemplate.convertAndSend(
+                RabbitConfig.ORDER_EXCHANGE,
+                RabbitConfig.ORDER_CREATED_ROUTING_KEY,
+                event
+        );
+        return savedOrder;
     }
 
     @Transactional
